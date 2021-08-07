f98a9d3bec2330c14787e87bf56148b51e66f50d
Rayray122 <88519182+Rayray122@users.noreply.github.com>
2021-08-07 10:27:21 +0530

    Create __main__.py

--- KURUMIBOT/__main__.py
+++ KURUMIBOT/__main__.py
@@ -205,7 +205,7 @@ def start(update: Update, context: CallbackContext):
                     ], 
                      [
                          InlineKeyboardButton(
-                             text="🔔 Updates of KURUMI 🔔",
+                             text="🔔 Updates of Noelle 🔔",
                              url="https://t.me/hiroiscool")
                     ], 
                      [
